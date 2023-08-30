d5cbcd2f3b2df34dde25bef26b4b0a225285b82c
Burness Scott III <bsiii102798@gmail.com>
2023-08-30 16:29:51 -0700

    navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
-//import {Link} from "react-dom";
-//import Navigation from "./Navigation";
+//import { Link } from "react-dom";
+import Navigation from "./Navigation";
 
 
 function Header() {
     return (
         <h1>Header</h1>
+
     )
 }
+<Navigation />
 
 
 export default Header
